a4e38964f9897c6915921e09028ef3704bb04a6a
Darkin <darkinb255@gmail.com>
2019-06-15 11:28:01 +0200

    Osclate

---
+++ Assets/Osclate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Osclate : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float mFactor;
+    public float mRange = 20;
+    Vector3 mVector;
+    Vector3 initPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.position;
+        print(initPos.ToString());
+        mVector = Vector3.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = initPos + mVector * mFactor * mRange ;
+        print(transform.position.ToString());
+    }
+}
