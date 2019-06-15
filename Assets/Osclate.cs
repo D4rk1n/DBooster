de1e6dbb3d88bee3d36e0bf3d7071bea636ee393
Darkin <darkinb255@gmail.com>
2019-06-15 11:29:24 +0200

    Osclating enhance

--- Assets/Osclate.cs
+++ Assets/Osclate.cs
@@ -6,21 +6,19 @@ public class Osclate : MonoBehaviour
 {
     [Range(0, 1)]
     public float mFactor;
-    public float mRange = 20;
-    Vector3 mVector;
+    public Vector3 mVector;
     Vector3 initPos;
     // Start is called before the first frame update
     void Start()
     {
         initPos = transform.position;
         print(initPos.ToString());
-        mVector = Vector3.up;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = initPos + mVector * mFactor * mRange ;
+        transform.position = initPos + mVector * mFactor  ;
         print(transform.position.ToString());
     }
 }
