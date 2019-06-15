71f5f9aa4b787a30ec226a7bc893b202f3835756
Darkin <darkinb255@gmail.com>
2019-06-15 12:10:28 +0200

    Organizing Prefabs

--- Assets/Osclate.cs
+++ Assets/Osclate.cs
@@ -6,7 +6,8 @@ public class Osclate : MonoBehaviour
 {
     [Range(0, 1)]
     public float mFactor;
-    public Vector3 mVector;
+    public float mPeriod=2f;
+    public Vector3 mVector= new Vector3(0,10f,0);
     Vector3 initPos;
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,12 @@ public class Osclate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = initPos + mVector * mFactor  ;
-        print(transform.position.ToString());
-    }
+        if (mPeriod!=0)
+        {
+            float cycles = Time.time / mPeriod;
+            mFactor = Mathf.Sin(cycles) / 2f + 0.5f;
+            transform.position = initPos + mVector * mFactor;
+        }
+     }
+
 }
